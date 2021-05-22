465f9041aaa1110897b258a51d62774822888a45
Dmitrijv <dmitrij.vel@live.se>
2021-05-22 20:31:52 +0200

    add basic table

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,85 @@
-<script lang="ts">
-	export let name: string;
+<script>
+  import GpuTable from "./GpuTable.svelte";
+
+  import { onMount } from "svelte";
+
+  export let cardList = [];
+  export let isLoading = true;
+
+  async function loadLatestData() {
+    isLoading = true;
+    await fetch("http://dmitrijv.se/projects/rtx-buddy/php/cards.php")
+      .then((res) => res.json())
+      .then((data) => {
+        cardList = data;
+      });
+
+    isLoading = false;
+  }
+
+  onMount(async () => {
+    await loadLatestData();
+  });
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="nav-container">
+    <div class="nav">
+      <h1>RTX 3080 Buddy</h1>
+      <button type="button" class="btn btn-light shadow-none" on:click={loadLatestData}>Update</button>
+    </div>
+  </div>
+  <div class="table-container">
+    {#if isLoading}
+      <!-- Spinner -->
+      <div class="text-center">
+        <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    {:else}
+      <GpuTable {cardList} />
+    {/if}
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .nav {
+    display: flex;
+    justify-content: space-between;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    width: 100%;
+    max-width: 700px;
+    padding: 10px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  button.btn-light {
+    background-color: #f0f0f0;
+  }
+
+  .nav h1 {
+    font-size: 26px;
+    line-height: 1.3;
+    color: #323c46;
+    padding: 0;
+    margin: 0;
+    text-align: left;
+  }
+
+  .nav-container {
+    background-color: white;
+    margin-bottom: 30px;
+    box-shadow: 0 -0.4rem 0.4rem 0.2rem rgb(0 0 0 / 50%);
+  }
+
+  .table-container {
+    width: 100%;
+    max-width: 700px;
+    box-shadow: rgba(149, 157, 165, 0.15) 0px 3px 6px 0px;
+    background-color: white;
+    margin: 10px auto;
+    padding: 10px;
+    border-radius: 6px;
+  }
 </style>
---
+++ src/GpuTable.svelte
@@ -0,0 +1,80 @@
+<script>
+  export let cardList = [];
+
+  function getClassByStatus(status) {
+    if (status == 0) return "in-stock";
+    else if (status == 1) return "in-stock";
+    else if (status == 2) return "incoming";
+    return "sold-out";
+  }
+
+  function getStatusLabel(status) {
+    if (status == 0) return "in stock";
+    else if (status == 1) return "in stock";
+    else if (status == 2) return "incoming";
+    else if (status == 3) return "delayed";
+    else if (status == 4) return "sold out";
+    else if (status == 5) return "blocked";
+    return "n/a";
+  }
+</script>
+
+<!-- GPU Table -->
+<table class="table table-sm table-hover">
+  <thead>
+    <tr>
+      <!-- <th scope="col">Status</th> -->
+      <th scope="col">Price</th>
+      <th scope="col">Card</th>
+      <th scope="col" class="d-none d-sm-table-cell">Restock</th>
+      <th scope="col" class="d-none d-sm-table-cell">Days</th>
+    </tr>
+  </thead>
+  <tbody class="inet-gpu-tbody">
+    {#each cardList as { id, productUrl, price, name, qty, status, restockDate, restockDays }, i}
+      <tr>
+        <!-- <td> <span class={getClassByStatus(status)}> {getStatusLabel(status)} </span></td> -->
+        <!-- class="d-none d-sm-table-cell" -->
+        <td>
+          <span class={getClassByStatus(status)}>{new Intl.NumberFormat("en-IN").format(price)} kr</span>
+        </td>
+        <td><a href={productUrl}>{name}</a></td>
+        <td class="d-none d-sm-table-cell">{restockDate}</td>
+        <td class="d-none d-sm-table-cell">{restockDays ? restockDays : ""}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<style>
+  span {
+    padding: 2px 3px;
+  }
+
+  .in-stock {
+    font-weight: bold;
+    color: white;
+    background-color: rgb(139, 170, 98);
+  }
+
+  .incoming {
+    font-weight: bold;
+    color: white;
+    background-color: rgb(222, 186, 90);
+  }
+
+  .sold-out {
+    font-weight: bold;
+    color: white;
+    background-color: rgb(164, 68, 65);
+  }
+
+  .store-logo {
+    max-height: 38px;
+    width: 100px;
+    margin-top: 20px;
+    background-color: rgb(0, 173, 219);
+    padding: 6px;
+    border-radius: 6px;
+  }
+</style>
