5f9e2bd638efed2cd95f6ee9cd8e78b2c2f912fa
Dmitrijv <dmitrij.vel@live.se>
2021-07-26 23:36:26 +0200

    add a 5 second timeout limit for computersalg curl requests

--- public/php/source/computersalg.php
+++ public/php/source/computersalg.php
@@ -43,6 +43,7 @@ function getComputersalgCards() {
       'Origin: '. $url,
       'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
     ));
+    curl_setopt($handle, CURLOPT_TIMEOUT, 5); //timeout in seconds
 
     curl_multi_add_handle($multi, $handle);
     $channels[$url] = $handle;
